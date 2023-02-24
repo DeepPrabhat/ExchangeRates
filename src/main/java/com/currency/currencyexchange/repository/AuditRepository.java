492bdc08b9e6e257bd8c7fe4c615dec5a86d0835
Deep Prabhat Tirkey <124681374+DeepPrabhat@users.noreply.github.com>
2023-02-25 02:34:39 +0530

    Delete src/main/java/com/currency/currencyexchange directory

--- src/main/java/com/currency/currencyexchange/repository/AuditRepository.java
+++
@@ -1,9 +0,0 @@
-package com.currency.currencyexchange.repository;
-
-import com.currency.currencyexchange.model.Audit_INFO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuditRepository extends JpaRepository<Audit_INFO,Long> {
-}
